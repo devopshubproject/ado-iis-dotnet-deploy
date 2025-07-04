b9bcd2153847e3531e0e79aee846f66601328175
premkumar-palanichamy <premkumar.palanichamy@yahoo.com>
2025-07-04 17:17:07 +0200

    update: provisioning without db

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -1,29 +1,32 @@
-### SQL Server and Database Configuration
+# ### SQL Server and Database Configuration
 
-resource "azurerm_mssql_server" "sql" {
-  name                         = "sqlserversecureiis"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "sqladmin"
-  administrator_login_password = "SecureP@ssw0rd!123"
+# resource "azurerm_mssql_server" "sql" {
+#   name                         = "sqlserversecureiis"
+#   resource_group_name          = azurerm_resource_group.rg.name
+#   location                     = azurerm_resource_group.rg.location
+#   version                      = "12.0"
+#   administrator_login          = "sqladmin"
+#   administrator_login_password = "SecureP@ssw0rd!123"
 
-  identity {
-    type = "SystemAssigned"
-  }
-}
-resource "azurerm_sql_database" "sqldb" {
-  name                = "iisappdb"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_mssql_server.sql.location
-  server_name         = azurerm_mssql_server.sql.name
-  sku_name            = "S0"
-}
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
+# resource "azurerm_mssql_database" "sqldb" {
+#   name                = "iisappdb"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_mssql_server.sql.location
+#   server_name         = azurerm_mssql_server.sql.name
+#   sku {
+#     name = "S0"
+#     tier = "Standard"
+#   }
+# }
 
-resource "azurerm_sql_firewall_rule" "allow_azure" {
-  name                = "AllowAzureServices"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_mssql_server.sql.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
-}
+# resource "azurerm_sql_firewall_rule" "allow_azure" {
+#   name                = "AllowAzureServices"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   server_name         = azurerm_mssql_server.sql.name
+#   start_ip_address    = "0.0.0.0"
+#   end_ip_address      = "0.0.0.0"
+# }
